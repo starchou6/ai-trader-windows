8b30afd6fc6de74110cb0f56ebc720f90536a64b
zhoujingweb3 <zhoujingweb3@gmail.com>
2026-01-03 14:05:07 +0900

    feat: replace status character by color Rectangle

---
+++ ai-trader-windows/Views/Converters/BoolToBrushConverter.cs
@@ -0,0 +1,22 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace AITrade.Converters
+{
+    public class BoolToBrushConverter : IValueConverter
+    {
+        public Brush TrueBrush { get; set; } = Brushes.Green;
+        public Brush FalseBrush { get; set; } = Brushes.Red;
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is bool b)
+                return b ? TrueBrush : FalseBrush;
+            return FalseBrush;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+            => throw new NotImplementedException();
+    }
+}
